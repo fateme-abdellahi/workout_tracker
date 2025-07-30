87c8369b1275727cc481c84a91afc002d3bc953e
Fateme Abdellahi <fateme.abdellahii@gmail.com>
2025-07-30 13:57:03 +0330

    add logout

--- user_auth/api/urls.py
+++ user_auth/api/urls.py
@@ -5,4 +5,5 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('signup/',views.SignUpView.as_view(),name='signup'),
     path('login/',obtain_auth_token,name='login'),
+    path('logout/',views.LogoutView.as_view(),name='logout'),
 ]
--- user_auth/api/views.py
+++ user_auth/api/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from . import serializers
+from rest_framework.authtoken.models import Token
+
 
 # Create your views here.
 
@@ -14,3 +16,12 @@ class SignUpView(APIView):
             token=serializer.save()
             return Response({"token":token},status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+class LogoutView(APIView):
+    def get(self, request):
+        # try:
+            token=Token.objects.get(user=request.user)
+            token.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        # except:
+            # return Response(status=status.HTTP_400_BAD_REQUEST)

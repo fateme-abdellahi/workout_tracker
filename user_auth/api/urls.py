12feae24501e5727c7209256db3b6ed857cde730
Fateme Abdellahi <fateme.abdellahii@gmail.com>
2025-07-30 13:29:07 +0330

    add login

--- user_auth/api/urls.py
+++ user_auth/api/urls.py
@@ -4,4 +4,5 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('signup/',views.SignUpView.as_view(),name='signup'),
+    path('login/',obtain_auth_token,name='login'),
 ]

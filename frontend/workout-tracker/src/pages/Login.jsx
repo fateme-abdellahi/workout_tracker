1748f8a9bc0c31c0cd495578211d43dad3cc7efc
Fateme Abdellahi <fateme.abdellahii@gmail.com>
2025-10-27 11:21:49 +0330

    add formik

--- frontend/workout-tracker/src/pages/Login.jsx
+++ frontend/workout-tracker/src/pages/Login.jsx
@@ -1,14 +1,33 @@
+import { Field, ErrorMessage, Form, Formik } from 'formik'
+import * as yup from 'yup'
+import '../assets/css/utils/form.css'
+
 const LoginPage = () => {
-    return <>
-        <form>
+    const handleSubmit = async (values) => {
+        console.log(values)
+    }
+
+    const valiadationSchema = yup.object({
+        username: yup.string().min(2, "username must be atleast 2 characters").max(20, "username can't be more than 20 characters").required("username is required"),
+        password: yup.string().min(8, "password length can not be less than 8").max(100, "password is too long").required("password is required"),
+    })
+
+    return <Formik initialValues={{
+        username: "",
+        password: "",
+    }} validationSchema={valiadationSchema} onSubmit={handleSubmit}>
+        {({ isSubmitting }) => (<Form>
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" required />
+            <Field type="text" name="username" id="username" required />
+            <ErrorMessage name='username' component='div' className='formFieldErrorMessage' />
 
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" required />
+            <Field type="password" name="password" id="password" required />
+            <ErrorMessage name='password' component='div' className='formFieldErrorMessage' />
 
-            <button type='submit'>Sign in</button>
-        </form>
-    </>
+            <button type='submit'>Sign up</button>
+        </Form>)
+        }
+    </Formik>
 }
 export default LoginPage
--- frontend/workout-tracker/src/pages/Register.jsx
+++ frontend/workout-tracker/src/pages/Register.jsx
@@ -1,20 +1,47 @@
+import { Field, ErrorMessage, Form, Formik } from 'formik'
+import * as yup from 'yup'
+import '../assets/css/utils/form.css'
+
 const RegisterPage = () => {
-    return <>
-        <form>
+
+    const handleSubmit = async (values) => {
+        console.log(values)
+    }
+
+    const valiadationSchema = yup.object({
+        username: yup.string().min(2, "username must be atleast 2 characters").max(20, "username can't be more than 20 characters").required("username is required"),
+        password: yup.string().min(8, "password length can not be less than 8").max(100, "password is too long").required("password is required"),
+        password2: yup.string().oneOf([yup.ref("password")], "passwords must match").required("this field is required"),
+        email: yup.string().email("invalid email").required("email is required")
+
+    })
+
+    return <Formik initialValues={{
+        username: "",
+        password: "",
+        password2: "",
+        email: "",
+    }} validationSchema={valiadationSchema} onSubmit={handleSubmit}>
+        {({ isSubmitting }) => (<Form>
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" required />
+            <Field type="text" name="username" id="username" required />
+            <ErrorMessage name='username' component='div' className='formFieldErrorMessage' />
 
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" required />
+            <Field type="password" name="password" id="password" required />
+            <ErrorMessage name='password' component='div' className='formFieldErrorMessage' />
 
             <label htmlFor="password2">Confirm Password</label>
-            <input type="password" id="password2" required />
+            <Field type="password" id="password2" name="password2" required />
+            <ErrorMessage name='password2' component='div' className='formFieldErrorMessage' />
 
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" required />
+            <Field type="email" id="email" name="email" required />
+            <ErrorMessage name='email' component='div' className='formFieldErrorMessage' />
 
             <button type='submit'>Sign up</button>
-        </form>
-    </>
+        </Form>)
+        }
+    </Formik>
 }
 export default RegisterPage
